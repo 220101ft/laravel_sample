6c10b4368cce685280a19bd29abb80431a4303f0
220101ft <220101ft@yse-c.net>
2023-12-01 11:50:08 +0900

    update

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -79,9 +79,9 @@ class ItemController extends Controller
         // 商品IDから商品データを取得
         // SELECT * FROM items WHERE id = xx;
         $item = Item::find($id);
-        dd($item);
+        $data['item'] = $item;
         // 編集画面を表示
-        
+        return view('item.edit', $data);
     }
 
     /**
@@ -89,7 +89,19 @@ class ItemController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->all();
+        // dd($data);
+        // UPDATE items SET price = xxx WHERE id = xx;
+        // 1.
+        // unset($data['_token']);
+        // Item::where('id', $id)->update($data);
+        // 2.
+        // SELECT * FROM items WHERE id = xx;
+        // UPDATE items SET price = xxx WHERE id = xx;
+        Item::find($id)->fill($data)->save();
+
+        // リダイレクト
+        return redirect(route('item.index', $id));
     }
 
     /**
