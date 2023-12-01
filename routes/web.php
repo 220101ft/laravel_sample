2e893f8f6d2e8cf9328d80f2901a7e1fa38e32ef
220101ft <220101ft@yse-c.net>
2023-12-01 11:03:54 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,21 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// HomeController
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/search', [HomeController::class, 'search'])->name('search');
+
+// ItemController
+Route::get('/item/', [ItemController::class, 'index'])->name('item.index');
+Route::get('/item/create', [ItemController::class, 'create'])->name('item.create');
+Route::post('/item/store', [ItemController::class, 'store'])->name('item.store');
+Route::get('/item/{id}', [ItemController::class, 'show'])->name('item.show');
 
 Route::get('/', function () {
+    // resources/views/welcome.blade.php ビューが表示
     return view('welcome');
 });
 
