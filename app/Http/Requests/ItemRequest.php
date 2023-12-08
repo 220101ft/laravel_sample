2e98667438d278b59899dfe4d48963829d8c60a6
220101ft <220101ft@yse-c.net>
2023-12-08 11:02:38 +0900

    update

--- app/Http/Requests/ItemRequest.php
+++ app/Http/Requests/ItemRequest.php
@@ -27,4 +27,14 @@ class ItemRequest extends FormRequest
             'price' => ['required', 'integer', 'min:0', 'max:100000'],
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '商品名を入力してください',
+            'price.required' => '価格を入力してください',
+            'price.integer' => '価格を数値で入力してください',
+            'price.min' => '価格を数値で入力してください',
+        ];
+    }
 }
